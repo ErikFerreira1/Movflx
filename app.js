1d111a1d073d0a92970b3442c305e92827aee316
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-06-05 10:37:14 -0300

    .

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 const User = require("./models/User");
+
 
 app.use(express.static(path.join(__dirname, "Front-end")));
 
