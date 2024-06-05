ec8e03fa04a35c59a394d422b63e77e91381f4f1
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-06-05 10:22:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -131,13 +131,12 @@ app.post("/auth/login", async (req, res) => {
 
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
-
+const PORT = process.env.PORT || 3000;
 mongoose
   .connect(
     `mongodb+srv://${dbUser}:${dbPassword}@authjwt.pjqqpic.mongodb.net/movflx`
   )
-  .then(() => {
-    app.listen(3000);
-    console.log("Conectou ao banco");
+  .then(PORT, () => {
+    console.log("Conectou ao banco. Porta:", PORT);
   })
   .catch((err) => console.log(err));
